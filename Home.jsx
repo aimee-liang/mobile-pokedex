cab47a6e384ff504f975aa58b1ce7dd4eddaf42f
aimee-liang <aimeeeliang@gmail.com>
2021-06-07 16:39:24 -0400

    Framework for Pokedex

---
+++ Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Stylesheet} from 'react-native'
+import {NativeRouter, Route} from 'react-router-native'
+
+export default function Home (){
+
+    const styles = Stylesheet.create({
+        
+    })
+}
