2538ca721001e8e717b0063a7478f47c527607d0
aimee-liang <aimeeeliang@gmail.com>
2021-06-07 16:45:48 -0400

    Home component with Routing

--- Home.jsx
+++ Home.jsx
@@ -5,6 +5,14 @@ import {NativeRouter, Route} from 'react-router-native'
 export default function Home (){
 
     const styles = Stylesheet.create({
-        
+        flex: 1,
+        alignItems: "center"
     })
+
+    return (
+        <NativeRouter>
+            <Route exact path="/pokemon" />
+
+        </NativeRouter>
+    )
 }
