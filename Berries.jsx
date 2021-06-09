24fd0b9951ecb95c42fd7d88cc56af8f7853fb57
aimee-liang <aimeeeliang@gmail.com>
2021-06-08 22:29:03 -0400

    Basic Berries.jsx

--- Berries.jsx
+++ Berries.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { render } from 'react-dom'
 import {Stylesheet} from 'react-native'
+import Berry from "./Berry"
 
 export default function Berries(){
+    const styles = Stylesheet.create({
+        container: {
+            flex: 1
+        }
+    })
 
+    const [allBerries, setAllBerries] = useState([])
+    const berriesUrl = `https://pokeapi.co/api/v2/berry/${i}`
+
+    useEffect(() => {
+        fetchBerries()
+    }, [])
+
+    const fetchBerries = () => {
+        for (let i = 0; i <= 100; i++){
+            fetch(berriesUrl)
+                .then(response => response.json())
+                .then(data => setAllBerries([allBerries, ...data]))
+        }
+    }
+
+    const renderBerries = () => {
+        return allBerries.map((berry) => {
+            <Berry key={berry.id} berry={berry} />
+        })
+    }
+
+    return (
+        <View style={styles.container}>
+            {renderBerries()}
+        </View>
+    )
 }
