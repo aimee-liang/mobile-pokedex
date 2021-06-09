0b04dc5d440c26d55a71a03ac5c651e4c0dcc69d
aimee-liang <aimeeeliang@gmail.com>
2021-06-08 21:41:06 -0400

    Added frontSprite styling

--- Individual.jsx
+++ Individual.jsx
@@ -5,12 +5,19 @@ export default function Individual(props){
     const styles= Stylesheet.create({
         container: {
             flex: 1
+        },
+        frontSprite:{
+            height: "200",
+            width: "200"
+        },
+        pokeName: {
+            
         }
     })
 
     return (
         <View style={styles.container}>
-            <Image>{props.pokemon["sprites"]["front_default"]}</Image>
+            <Image style={styles.frontSprite}>{props.pokemon["sprites"]["front_default"]}</Image>
             <Text>{props.pokemon.name}</Text>
             <Text>{props.pokemon["types"]["type"]["name"]}</Text>
         </View>
