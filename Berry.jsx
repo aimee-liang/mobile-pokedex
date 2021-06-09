7cf196f7295ab4139ffd3e3c9ef79a5a25d86105
aimee-liang <aimeeeliang@gmail.com>
2021-06-09 12:13:30 -0400

    Berry.jsx

--- Berry.jsx
+++ Berry.jsx
@@ -1,2 +1,19 @@
 import React from 'react'
+import {Stylesheet, View, Text} from 'react-native'
 
+export default function Berry(props){
+    const styles= Stylesheet.create({
+        container: {
+            flex: 1
+        },
+        berryName: {
+            fontWeight: "bold"
+        }
+    })
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.berryName}>{props.berry["name"]}</Text>
+        </View>
+    )
+}
