10ab277977b3566fe77c855608af0097a2f3ab2a
aimee-liang <aimeeeliang@gmail.com>
2021-06-10 00:00:55 -0400

    Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Stylesheet} from 'react-native'
+import {Stylesheet, View, Text} from 'react-native'
 import {NativeRouter, Route} from 'react-router-native'
 
 export default function Home (){
@@ -8,12 +8,23 @@ export default function Home (){
         container: {
             flex: 1,
             alignItems: "center"
+        },
+        header: {
+            backgroundColor: "red"
+        },
+        pokedexText: {
+            color: "yellow",
+            fontSize: 26
         }
     })
 
     return (
         <NativeRouter>
             <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.pokedexText}>Pokedex</Text>
+                </View>
+
                 <Route exact path="/pokemon" render={() => <Pokemon />} />
                 <Route exact path="/berries" render={() => <Berries/>} />
             </View>
