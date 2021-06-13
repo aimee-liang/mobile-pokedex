fc4dd6b219259321e441104844784c8e851c6185
aimee-liang <aimeeeliang@gmail.com>
2021-06-12 23:23:51 -0400

    Modal for SearchPokemon.jsx

--- SearchPokemon.jsx
+++ SearchPokemon.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { StyleSheet, View, TextInput } from 'react-native'
+import { StyleSheet, View, TextInput, Modal } from 'react-native'
 
 export default function SearchPokemon(){
     const styles = StyleSheet.create({
@@ -9,10 +9,11 @@ export default function SearchPokemon(){
     })
 
     const [pokemonName, setPokemonName] = useState("")
+    const [modalVisible, setModalVisible] = useState(false)
 
     return (
         <View style={styles.container}>
-            <TextInput onChangeText={}></TextInput>
+            <TextInput onChangeText={setPokemonName} value={pokemonName} placeholder="Enter a Pokemon Name"></TextInput>
         </View>
     )
 }
