f8a6e971c78ba1f1a988228612299f4b3cd3b0ed
aimee-liang <aimeeeliang@gmail.com>
2021-06-16 16:20:59 -0400

    Removed Modal from SearchPokemon.jsx

--- SearchPokemon.jsx
+++ SearchPokemon.jsx
@@ -1,22 +1,14 @@
 import React, {useState} from 'react'
-import { StyleSheet, View, TextInput, Modal, Button, Keyboard } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Keyboard } from 'react-native'
 
 export default function SearchPokemon(){
     const styles = StyleSheet.create({
         container:{
             flex: 1
-        },
-        modalContainer:{
-
         }
     })
 
     const [pokeName, setPokeName] = useState("")
-    const [modalVisible, setModalVisible] = useState(false)
-
-    const updateVisibility = () => {
-        setModalVisible(!modalVisible)
-    }
 
     const searchBarHandler = () => {
         props.setPokemonName(pokeName)
@@ -25,12 +17,9 @@ export default function SearchPokemon(){
     }
 
     return (
-        <>
-            <Modal visible={modalVisible} onPress={updateVisibility}>Looking for a Pokemon? </Modal>
-            <View style={styles.container}>
-                <TextInput onChangeText={setPokeName} value={pokeName} placeholder="Enter a Pokemon Name"></TextInput>
-                <Button onPress={searchBarHandler} title="🔎"/>
-            </View>
-        </>
+        <View style={styles.container}>
+            <TextInput onChangeText={setPokeName} value={pokeName} placeholder="Enter a Pokemon Name"></TextInput>
+            <Button onPress={searchBarHandler} title="🔎" />
+        </View>
     )
 }
